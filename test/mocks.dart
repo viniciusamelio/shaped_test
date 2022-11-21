2dbaa8bedeb954c333f692b20233be30777119e3
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-11-21 20:50:23 -0300

    refactor: extracting mocks

---
+++ test/mocks.dart
@@ -0,0 +1,7 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:shaped_test/core/base_domain/base_exception.dart';
+import 'package:shaped_test/domain/repositories/auth_repository.dart';
+
+class MockedException extends Mock implements BaseException {}
+
+class MockedAuthRepository extends Mock implements AuthRepository {}
--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,11 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:shaped_test/core/base_domain/base_exception.dart';
+
+export 'mocks.dart';
 
 expectLeft<T>(Either result) {
   expect(result.isLeft(), isTrue);
   expect(result.fold((l) => l, (r) => null), isA<T>());
 }
-
-class MockedException extends Mock implements BaseException {}
