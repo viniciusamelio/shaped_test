28b182fae719c98d9786e9350088e40c82a7025b
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-11-21 20:50:15 -0300

    refactor: extracting production code

--- lib/domain/usecases/signin_usecase.dart
+++ lib/domain/usecases/signin_usecase.dart
@@ -1,5 +1,30 @@
+import 'package:fpdart/fpdart.dart';
 import 'package:shaped_test/core/base_domain/base_usecase.dart';
 import 'package:shaped_test/domain/entities/auth_credential.dart';
 import 'package:shaped_test/domain/entities/user.dart';
+import 'package:string_validator/string_validator.dart';
+
+import '../../core/shared/types.dart';
+import '../errors/auth_exceptions.dart';
+import '../repositories/auth_repository.dart';
 
 abstract class SignInUsecase implements BaseUsecase<AuthCredential, User> {}
+
+class SignInUsecaseImpl implements SignInUsecase {
+  SignInUsecaseImpl({
+    required AuthRepository authRepository,
+  }) : _authRepository = authRepository;
+
+  final AuthRepository _authRepository;
+
+  @override
+  Output<User> call(AuthCredential input) async {
+    if (!isEmail(input.email)) {
+      return Left(InvalidEmail());
+    } else if (input.password.isEmpty) {
+      return Left(InvalidPassword());
+    }
+    final userOrError = await _authRepository.signin(input);
+    return userOrError;
+  }
+}
--- test/domain/usecases/signin_usecase_test.dart
+++ test/domain/usecases/signin_usecase_test.dart
@@ -3,35 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:shaped_test/domain/entities/auth_credential.dart';
-import 'package:shaped_test/core/shared/types.dart';
 import 'package:shaped_test/domain/entities/user.dart';
 import 'package:shaped_test/domain/errors/auth_exceptions.dart';
 import 'package:shaped_test/domain/repositories/auth_repository.dart';
 import 'package:shaped_test/domain/usecases/signin_usecase.dart';
-import 'package:string_validator/string_validator.dart';
 
 import '../../test_utils.dart';
-
-class MockedAuthRepository extends Mock implements AuthRepository {}
-
-class SignInUsecaseImpl implements SignInUsecase {
-  SignInUsecaseImpl({
-    required AuthRepository authRepository,
-  }) : _authRepository = authRepository;
-
-  final AuthRepository _authRepository;
-
-  @override
-  Output<User> call(AuthCredential input) async {
-    if (!isEmail(input.email)) {
-      return Left(InvalidEmail());
-    } else if (input.password.isEmpty) {
-      return Left(InvalidPassword());
-    }
-    final userOrError = await _authRepository.signin(input);
-    return userOrError;
-  }
-}
 
 void main() {
   late SignInUsecase sut;
